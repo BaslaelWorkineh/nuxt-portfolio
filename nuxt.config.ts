e82ce6f2dc28366740c4f132f6df76c5b643c372
BaslaelWorkineh <baslaworku@gmail.com>
2024-12-15 12:54:03 +0300

    dist added for ci/cd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,13 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  nitro: {
+    node: true, // Ensures the server runs as a Node.js application
+    preset: 'node-server', // Specific for cPanel Node.js hosting
+    minify: true, // Minifies the output for reduced memory usage
+  },
 
   compatibilityDate: '2024-12-15',
+  ssr: true,
+  target: 'static'
 })
